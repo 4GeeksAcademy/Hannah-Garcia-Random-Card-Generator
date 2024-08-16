3c5ebad139424aa5623736d4c83f0d3f1baef438
Hannah Garcia <hannah.kacso@gmail.com>
2024-08-16 19:08:15 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,40 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+// window.onload = function() {
+//   //write your code here
+//   console.log("Hello Rigo from the console!");
+// };
+const suits = ['♥', '♠', '♣', '♦'];
+const values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'K', 'Q', 'J', 'A'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+const cardElement = document.getElementById('card');
+const topLeftElement = document.querySelector('.top-left');
+const centerElement = document.querySelector('.center');
+const bottomRightElement = document.querySelector('.bottom-right');
+const generateButton = document.getElementById('generate');
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function getRandomCard() {
+    const suit = suits[Math.floor(Math.random() * suits.length)];
+    const value = values[Math.floor(Math.random() * values.length)];
+    const color = (suit === '♥' || suit === '♦') ? 'red' : 'black';
+    return { suit, value, color };
+}
+
+function displayCard(card) {
+    cardElement.className = `card ${card.color}`;
+    topLeftElement.innerHTML = `${card.suit}`;
+    centerElement.innerHTML = `${card.value}`;
+    bottomRightElement.innerHTML = `${card.suit}`;
+}
+
+function generateNewCard() {
+    const card = getRandomCard();
+    displayCard(card);
+}
+
+// Generate a new card every 10 seconds
+setInterval(generateNewCard, 10000);
+
+// Generate a new card on button click
+generateButton.addEventListener('click', generateNewCard);
+
+// Generate a new card on page load
+window.onload = generateNewCard;
